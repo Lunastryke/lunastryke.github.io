5b424055ccd73050f2bdfc6a513c4b2f1ca640f5
Xuan Hao <xuanhaolim.lxh@gmail.com>
2022-03-06 01:42:23 +0800

    Changed optimisation for images to support next export to static html

--- components/layout.js
+++ components/layout.js
@@ -16,26 +16,17 @@ export default function Layout({ children, home }) {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
+            <img src="./images/profile.jpg"
               height={144}
               width={144}
-              alt={name}
-            />
+              className={utilStyles.borderCircle}
+              alt={name}/>
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
